c415bf5fc5f879fc43e75dc2752f8c6addc5ce4f
heart <>
2021-12-13 14:33:09 +0200

    move StringCompiler

--- src/Heart.Parsing/Parsing/StringCompiler.cs
+++ src/Heart.Parsing/Parsing/StringCompiler.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Heart.Parsing;
 using Heart.Parsing.Patterns;
 
-namespace Heart.Tests
+namespace Heart.Parsing
 {
     public static class StringCompiler
     {
